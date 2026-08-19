2f29bb2c446c8263febe3e937734228887217378
bryanvaldezalmeida-bit <bryan.valdez.almeida@escola.pr.gov.br>
2026-08-19 10:55:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+  window.location.assign("https://godzilla.com/?srsltid=AfmBOoo-ZuSQjbPHpDIHvCMxvubEVnMuD1ofcA6c5RH6_5bqUmYjQ0dp");
+
+let  nome = prompt("Qual é o seu nome?");
+if (nome) {
+    document.("titulo-boas-vindas").innerText = "Olá, " + nome + "! Bem-vindo ao site do Godzilla.":
+}
